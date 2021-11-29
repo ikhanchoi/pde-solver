e2fed076265b0d767c9e043fca3615d34d95ecb7
Ikhan Choi <dlrgks623@gmail.com>
2021-11-29 15:23:18 +0900

    21/11/29

--- python/ising/mcmc.py
+++ python/ising/mcmc.py
@@ -5,15 +5,17 @@ sys.path.insert(0, '..')
 import visualize
 
 
-L = 100
-n_step = 10000
+L = 32
+n_step = 1000
 
-def nbr(x,y):
-	return [((x+1)%L, y), ((x-1)%L, y), (x, (y+1)%L), (x, (y-1)%L)]
+dx = [1,0,-1,0,0]
+dy = [0,1,0,-1,0]
+nbr = {(x,y) :  [ ((x+dx[i]) % L, (y+dy[i]) % L) for i in range(5) ] for
+	   (x,y) in [ (p//L, p%L) for p in range(L**2) ] }
 
 
 
-t = np.arange(2, 3, 0.1)
+t = np.linspace(0.2, 4.0, 20)
 T = t.size
 beta = 1.0 / t
 M = np.zeros(T)
@@ -23,24 +25,29 @@ for n in range(T):
 	S = np.random.choice([1,-1], size = (L, L))
 	for step in range(n_step):
 		r = tuple(np.random.randint(L, size = 2))
-		pocket, cluster = [r], [r]
+		F_old, cluster = [r], [r]
 
-		while pocket:
-			for p in pocket:
-				for q in nbr(*p):
+		while F_old:
+			F_new = []
+			for p in F_old:
+				for q in nbr[p]:
 					if S[q] == S[p] and q not in cluster:
-						if np.random.uniform(0.0,1.0) < 1 - np.exp(-2*beta[n]):
-							pocket.append(q)
+						if np.random.uniform(0,1) < 1 - np.exp(-2*beta[n]):
+							F_new.append(q)
 							cluster.append(q)
-			pocket.remove(p)
+			F_old = F_new
 		for q in cluster:
 			S[q] *= -1
-		print("t=",t[n],",   ",step,"/",n_step)
+		if step % 10 == 0:
+			print("t=%.1f" % t[n], ",   ", step, "step /", n_step)
 
 	M[n] = sum(sum(S))
 	image.append(S)
 
-print(image[1])
+for n in range(T):
+	print("Tiem", t[n], ":")
+	print(image[n])
+
 print(np.abs(M) / L**2)
 visualize.Plot2d_array(np.abs(M) / L**2,t)
 
